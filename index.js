1172434e53d2d105575de3967940a8c5bd3bba02
patrickwide <patrickwide254@gmail.com>
2023-03-15 02:02:54 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+// Define the currentUser variable
+let currentUser = "John";
+
+// Define the welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting variable
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
