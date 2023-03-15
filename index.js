65694e9ef9e56fe4b708bbdcd27f00d42a7e3d57
patrickwide <patrickwide254@gmail.com>
2023-03-15 13:06:39 +0300

    Hot fix: add split

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 // Define the currentUser variable
-let currentUser = "John";
+let currentUser = "John smith";
 
 // Define the welcomeMessage variable
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
@@ -9,5 +9,5 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 // Define the shortGreeting variable
-const firstInitial = currentUser[0];
+const firstInitial = currentUser.split(" ")[0];
 const shortGreeting = `Welcome, ${firstInitial}!`;
